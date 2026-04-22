74e319789e17c11bd29184135aa491d2b84e3d8f
LSkhosana07 <lesedi@metmetric.co.za>
2026-04-22 13:19:18 +0200

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+//  function toggleMenu() {
+//             var menu = document.getElementById("mobileMenu");
+//             menu.classList.toggle("active");
+//     }
+
+function toggleMenu() {
+    var menu = document.getElementById("mobileMenu");
+    menu.classList.toggle("active");
+}
